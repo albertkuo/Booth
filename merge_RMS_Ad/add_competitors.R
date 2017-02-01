88a4e521a6a2bf460e4f7cdd43e0cc3aa2d40a79
Albert Kuo <albertkuo@uchicago.edu>
2017-01-31 22:00:18 -0600

    Modified code to be easier to read

--- merge_RMS_Ad/add_competitors.R
+++ merge_RMS_Ad/add_competitors.R
@@ -1,7 +1,7 @@
 # add_competitors.R
 # -----------------------------------------------------------------------------
 # Author:             Albert Kuo
-# Date last modified: December 22, 2016
+# Date last modified: January 31, 2016
 #
 # This is an R script that adds competitor values
 # after data is built the first time with merge_RMS_Ad.R
@@ -12,80 +12,75 @@
 library(data.table)
 library(bit64)
 
-RMS_input_dir = "/grpshares/hitsch_shapiro_ads/data/RMS/Brand-Aggregates"
+num_competitors = 3 # Number of competitors whose prices/promotions we want to control for
 output_dir = "/grpshares/hitsch_shapiro_ads/data/RMS_Ad"
-dir_names = list.dirs(RMS_input_dir, full.names=F, recursive=F)
+dir_names = list.dirs(output_dir, full.names=F, recursive=F)
+
+# Metadata for finding competitors
+load('/grpshares/hitsch_shapiro_ads/data/RMS/Meta-Data/Products-Corrected.RData')
+load('/grpshares/hitsch_shapiro_ads/data/RMS/Meta-Data/Meta-Data-Corrected.RData')
+meta_data[, product_module_code:=NULL]
+prod_meta = merge(products, meta_data)
+brands_RMS = prod_meta[,.(rev_sum = sum(revenue_RMS)), 
+                       by=c("brand_code_uc","brand_descr",
+                            "product_module_code", "product_module_descr")]
+brands_RMS = brands_RMS[order(-rev_sum)]
+top100_brandcodes = brands_RMS[1:100]$brand_code_uc
 
 # Traverse through directories
-#for(k in 1:length(dir_names)){ # Soft drinks = 1484
-for(k in 1:1){
+for(k in 1:length(dir_names)){ 
+#for(k in 1:1){
   print(k)
-  #dir_name = dir_names[[k]]
-  dir_name = "1484"
+  dir_name = dir_names[[k]]
+  #dir_name = "1484" # Soft drinks = 1484, Paper towels = 7734, Yogurt = 3603, Diapers = 8444
   print(dir_name)
-  print("Adding competitor columns...")
-  top_competitors = c("531429","616141","602144","543721")
-  competitors_data = list()
-  merged_filenames = list.files(file.path(output_dir,dir_name), full.names=T)
+  merged_filenames = list.files(file.path(output_dir, dir_name), full.names=T)
+  built_brands = sapply(merged_filenames, function(x){sub(pattern = "(.*?)\\..*$", replacement = "\\1", basename(x))})
+  built_brands = sapply(built_brands, as.integer)
+  competitors = brands_RMS[product_module_code == as.integer(dir_name) &
+                             brand_code_uc %in% built_brands][order(-rev_sum)]$brand_code_uc
+  competitors = sapply(competitors, as.character)
+  top_competitors = sapply(competitors[1:min(num_competitors+1, length(competitors))], as.character)
+  print(top_competitors)
+  #top_competitors = c("531429","616141","602144","543721")
+  competitors_datalist = list()
+  
+  # Read competitors data directly
+  for(i in 1:length(top_competitors)){
+    competitor_brand = top_competitors[[i]]
+    competitor_DT = readRDS(paste0(file.path(output_dir, dir_name),"/",competitor_brand,".rds"))
+    competitors_datalist[[competitor_brand]] = copy(competitor_DT)
+  }
+  competitors_DT = rbindlist(competitors_datalist)
+
+  # Read brands in top 100 and add competitors
   if(length(merged_filenames)>1){
     for(i in 1:length(merged_filenames)){
       merged_filename = merged_filenames[[i]]
-      RMS_brand_code = sub(pattern = "(.*?)\\..*$", replacement = "\\1", basename(merged_filename))
-      brand_data = readRDS(merged_filename)
-      if(RMS_brand_code %in% top_competitors){
-        competitor_data = brand_data[, c("Week","store_code_uc","price","promo_dummy"), with=F]
-        setkeyv(competitor_data, c("Week", "store_code_uc"))
-        competitor_data = unique(competitor_data)
-        setnames(competitor_data, c("Week", "store_code_uc", paste(c("price", "promo_dummy"), RMS_brand_code, sep="_")))
-        competitors_data[[RMS_brand_code]] = competitor_data
-      }
-      brand_data = brand_data[, c("Week", "dma_code", "National_GRP", "Local_GRP", "Total_GRP",
-                                  "National_occ", "Local_occ", "Total_occ"), with=F]
-      if(i==1){
-        sum_data = copy(brand_data)
-      } else {
-        sum_data = rbind(sum_data, brand_data)
-        sum_data = sum_data[,.(National_GRP = sum(National_GRP, na.rm=T), 
-                               Local_GRP = sum(Local_GRP, na.rm=T),
-                               Total_GRP = sum(Total_GRP, na.rm=T),
-                               National_occ = sum(National_occ, na.rm=T),
-                               Local_occ = sum(Local_occ, na.rm=T),
-                               Total_occ = sum(Total_occ, na.rm=T)), by=c("Week", "dma_code")]
-        print(summary(sum_data$Total_GRP))
-      }
-    }
-    setnames(sum_data, c("Week", "dma_code", "National_GRP_sum", "Local_GRP_sum",
-                         "Total_GRP_sum", "National_occ_sum", "Local_occ_sum", "Total_occ_sum"))
-    #print(head(sum_data))
-    
-    # Add competitor columns
-    for(i in 1:length(merged_filenames)){
-      print(i)
-      n_competitors = 0
-      merged_filename = merged_filenames[[i]]
-      RMS_brand_code = sub(pattern = "(.*?)\\..*$", replacement = "\\1", basename(merged_filename))
-      brand_data = readRDS(merged_filename)
-      brand_data = merge(brand_data, sum_data, by=c("Week","dma_code"))
-      brand_data[, `:=`(National_GRP_rival = National_GRP_sum-National_GRP,
-                        Local_GRP_rival = Local_GRP_sum-Local_GRP,
-                        Total_GRP_rival = Total_GRP_sum-Total_GRP,
-                        National_occ_rival = National_occ_sum-National_occ,
-                        Local_occ_rival = Local_occ_sum-Local_occ,
-                        Total_occ_rival = Total_occ_sum-Total_occ)]
-      brand_data[, (c("National_GRP_sum", "Local_GRP_sum", "Total_GRP_sum", 
-                      "National_occ_sum", "Local_occ_sum", "Total_occ_sum")):=NULL, with=F]
-      for(brand in names(competitors_data)){
-        if(brand!=RMS_brand_code & n_competitors<3){
-          print(RMS_brand_code)
-          print(names(competitors_data[[brand]]))
-          brand_data[, grep("price|promo_dummy", names(competitors_data[[brand]]), value=T):=NULL, with=F]
-          brand_data = merge(brand_data, competitors_data[[brand]], by=c("Week","store_code_uc"), all.x=T)
-          n_competitors = n_competitors+1
+      RMS_brand = sub(pattern = "(.*?)\\..*$", replacement = "\\1", basename(merged_filename))
+      if(RMS_brand %in% top100_brandcodes){
+        if(RMS_brand %in% top_competitors){
+          brand_data = copy(competitors_DT)
+        } else {
+          brand_data = readRDS(merged_filename)
+          brand_data = rbind(brand_data, competitors_DT[brand_code_uc_corrected %in% top_competitors[-length(top_competitors)]])
         }
+        print(unique(brand_data$brand_code_uc_corrected))
+        brand_data[, brand_type:="comp"]
+        brand_data[brand_code_uc_corrected == RMS_brand, brand_type:="own"]
+        # dcast
+        brand_data = dcast(brand_data, dma_code + store_code_uc + week_end + year ~ brand_type,
+                           value.var = list("units", "price", "promo_percentage","Total_GRP","Total_occ"), 
+                           fun=list(mean, mean, mean, sum, sum))
+        brand_data[Total_GRP==0, Total_GRP:=NA]
+        brand_data[Total_occ==0, Total_occ:=NA]
+        brand_data[, units_mean_comp:=NULL]
+        names(brand_data) = sapply(names(brand_data), function(x){gsub("_mean|_sum","",x)})
+        setnames(brand_data, c("units_own"), c("units"))
+        print("Saving with competitors...")
+        saveRDS(brand_data, file=paste0(file.path(output_dir, dir_name), "/",
+                                        RMS_brand, ".rds"))
       }
-      print("Saving with competitors...")
-      saveRDS(brand_data, file=paste0(file.path(output_dir, dir_name), "/",
-                                      RMS_brand_code, ".rds"))
     }
   } else {
     print("No competitors")
--- merge_RMS_Ad/merge_RMS_Ad.R
+++ merge_RMS_Ad/merge_RMS_Ad.R
@@ -1,7 +1,7 @@
 # merge_RMS_Ad.R
 # -----------------------------------------------------------------------------
 # Author:             Albert Kuo
-# Date last modified: December 19, 2016
+# Date last modified: January 31, 2016
 #
 # This is an R script that merges RMS and Ad Intel based on brand, market, week
 # Run aggregate_RMS.R and ad_extract.R prior to this
@@ -11,13 +11,14 @@
 ## ================
 library(data.table)
 library(bit64)
+library(RcppRoll)
 
 run_grid = T
 if(!run_grid){
   dir_name = 1484
   brand_code = 531429
-  RMS_example = readRDS("Booth/merge_RMS_Ad/531429.rds")
-  Ad_example = readRDS("Booth/merge_RMS_Ad/201206_aggregated.rds")
+  RMS_data = readRDS("Booth/merge_RMS_Ad/531429.rds")
+  Ad_data = readRDS("Booth/merge_RMS_Ad/201206_aggregated.rds")
   load("Booth/merge_RMS_Ad/Stores.RData")        #stores
   load("Booth/merge_RMS_Ad/Store-Address.RData") #store_info
   zipborders = fread("Booth/county/zipborders.csv")
@@ -30,8 +31,9 @@ ad_output_dir = "/grpshares/hitsch_shapiro_ads/data/Ad_Intel"
 output_dir = "/grpshares/hitsch_shapiro_ads/data/RMS_Ad"
 
 load('/grpshares/hitsch_shapiro_ads/data/RMS/Meta-Data/Stores.RData')
-load('~/merge_metadata/Store-Address.RData')
-zipborders = fread('~/merge_metadata/zipborders.csv')
+store_dma_mapping = stores[, c("store_code_uc", "year", "dma_code"), with=F]
+setkey(store_dma_mapping)
+store_dma_mapping = unique(store_dma_mapping)
 string_matches = fread('~/merge_metadata/string_matches.csv')
 
 # Set parameters
@@ -39,170 +41,128 @@ weight = 0.9 # Weight for ad stock
 window = 52 # Window of T weeks for ad stock
 associated_brands = F # Indicator for including level 4 matches
 
-# Clean metadata
-stores = stores[, c("store_code_uc","year","store_zip3","fips_state_code","fips_county_code","dma_code"), with=F]
-store_dma_mapping = stores[, c("store_code_uc", "year", "dma_code"), with=F]
-setkey(store_dma_mapping)
-store_dma_mapping = unique(store_dma_mapping)
-store_info[, Year:=strtoi(substring(DATE, 0, 4), base=10)]
-store_info[, store_code_uc:=`Store ID UC`]
-store_info[, c("Store ID UC", "NAME", "PARENT", "BANNER"):=NULL]
-store_info[, rollyear:=Year]
-#setkey(zipborders, zip3, on_border, bordername)
-#zipborders = unique(zipborders)
-#zipborders[, zip:=NULL]
-
 dir_names = list.dirs(RMS_input_dir, full.names=F, recursive=F)
 
-#for(k in 1:length(dir_names)){ # Soft drinks = 1484
-for(k in 1:1){
+for(k in 1:length(dir_names)){ 
+#for(k in 1:1){
   print(k)
-  #dir_name = dir_names[[k]]
-  dir_name = "1484"
+  dir_name = dir_names[[k]]
+  #dir_name = "1484" # Soft drinks = 1484, Paper towels = 7734, Yogurt = 3603, Diapers = 8444
   print(dir_name)
   RMS_filenames = list.files(file.path(RMS_input_dir, dir_name), full.names=T)
-  brand_codes = list()
-  for(i in 1:length(RMS_filenames)){
-    RMS_filename = RMS_filenames[[i]]
-    brand_codes[[i]] = sub(pattern = "(.*?)\\..*$", replacement = "\\1", basename(RMS_filename))
-  }
   
   print("Reading RMS file...")
-  
   for(i in 1:length(RMS_filenames)){
     RMS_filename = RMS_filenames[[i]]
     print(RMS_filename)
-    RMS_brand_code = brand_codes[[i]]
-    RMS_example = readRDS(RMS_filename)
+    RMS_brand_code = sub(pattern = "(.*?)\\..*$", replacement = "\\1", basename(RMS_filename))
+    RMS_data = readRDS(RMS_filename)
     
     # Clean some data stuff on RMS side
-    RMS_example[, year:=year(week_end)]
-    RMS_example[, Week:=format(week_end,format="%W/%Y")]
-    RMS_example[, product_module_code:=strtoi(dir_name, base=10)]
-  
-    # Join to zip/DMA matching
-    RMS_example = merge(RMS_example, store_dma_mapping, by=c("store_code_uc", "year"))
-  
-    # Read in ad files
-    print("Reading in ad files...")
-    RMS_ad_filenames = list.files(file.path(ad_output_dir, "aggregated_extracts", dir_name),
-                                  pattern=RMS_brand_code, full.names=T)
-    ad_datalist = lapply(RMS_ad_filenames, fread)
-    Ad_example = rbindlist(ad_datalist)
-    # Select whether to include match_tier = 4 or not
-    if(!associated_brands & nrow(Ad_example)>0){
-      filter_brands = string_matches[brand_code_uc==RMS_brand_code & product_module_code==strtoi(dir_name) & match_tier!=4]$BrandCode
-      Ad_example = Ad_example[BrandCode %in% filter_brands]
-    }
-  
-    if(nrow(Ad_example)>0){
-      # Re-accumulate by week to fix month cross-over values
-      Ad_example = Ad_example[,.(National_GRP = as.numeric(sum(National_GRP, na.rm=T)),
-                                 Local_GRP = as.numeric(sum(Local_GRP, na.rm=T)),
-                                 National_occ = as.numeric(sum(National_occ, na.rm=T)),
-                                 Local_occ = as.numeric(sum(Local_occ, na.rm=T))),
-                                 by=c("BrandCode", "Week", "MarketCode")]
-  
-      # Fill in missing rows
-      print("Filling in missing rows...")
-      #print(tail(Ad_example))
-      #print(nrow(Ad_example))
-      Ad_example[, WeekDate:=as.Date(paste0("1/",Week),format="%w/%W/%Y")]
-      setkey(Ad_example, BrandCode, MarketCode, WeekDate)
-      Ad_example = Ad_example[CJ(unique(BrandCode), unique(MarketCode), seq(min(WeekDate), max(WeekDate), by=7))]
-      #print(tail(Ad_example))
-      #print(nrow(Ad_example))
-  
-      # Calculate Ad stock and Total values
-      # Assumption that all weeks need to be filled for correct weight
-      # Check that order is by weeks
-      print("Calculating ad stock...")
-      setorder(Ad_example, WeekDate)
-      # Under option (b), first create estimated data occurring before time interval using average
-      b=T
-      if(b){
-        simulated = Ad_example[, .(National_GRP = sum(National_GRP*(seq(.N)<=window))/window,
-                                   Local_GRP = sum(Local_GRP*(seq(.N)<=window))/window,
-                                   National_occ = sum(National_occ*(seq(.N)<=window))/window,
-                                   Local_occ = sum(Local_occ*(seq(.N)<=window))/window),
-                               by=c("BrandCode", "MarketCode")]
-        simulated = simulated[rep(seq(.N), each=window)]
-        simulated_weekdate = as.Date("01/01/1900",format="%w/%W/%Y")
-        simulated[, `:=`(WeekDate=simulated_weekdate, Week=NA)] #Note that simulated data does not have real Week values
-        # Assign all markets' codes
-        setcolorder(simulated, c("BrandCode", "MarketCode", "Week", "WeekDate",
-                                 "National_GRP", "Local_GRP", "National_occ", "Local_occ"))
-        setcolorder(Ad_example, c("BrandCode", "MarketCode", "Week", "WeekDate",
-                                  "National_GRP", "Local_GRP", "National_occ", "Local_occ"))
-        Ad_example = rbind(simulated, Ad_example)
+    RMS_data[, year:=year(week_end)]
+    RMS_data[, Week:=format(week_end,format="%W/%Y")]
+    RMS_data[, product_module_code:=strtoi(dir_name, base=10)]
+
+    # Read in ad file
+    print("Reading in ad file...")
+    ad_filename = paste0(file.path(ad_output_dir, "aggregated_extracts", dir_name),
+                         "/", RMS_brand_code, ".rds")
+    if(file.exists(ad_filename)){
+      Ad_data = readRDS(ad_filename)
+    
+      # Select whether to include match_tier = 4 or not
+      if(!associated_brands & nrow(Ad_data)>0){
+        filter_brands = string_matches[brand_code_uc==RMS_brand_code & 
+                                       product_module_code==strtoi(dir_name, base=10) & 
+                                       match_tier!=4]$BrandCode
+        Ad_data = Ad_data[BrandCode %in% filter_brands]
       }
+
+      if(nrow(Ad_data)>0){
+        # Fill in missing rows
+        print("Filling in missing rows...")
+        Ad_data[, WeekDate:=as.Date(paste0("1/",Week),format="%w/%W/%Y")]
+        setkey(Ad_data, BrandCode, MarketCode, WeekDate)
+        Ad_data = Ad_data[CJ(unique(BrandCode), unique(MarketCode), 
+                             seq(min(WeekDate), max(WeekDate), by=7))]
+    
+        # Calculate Ad stock and Total values
+        print("Calculating ad stock...")
+        setorder(Ad_data, WeekDate)
+        # Under option (b), first create estimated data occurring before time interval using average
+        b=T
+        if(b){
+          simulated = Ad_data[, .(National_GRP = sum(National_GRP*(seq(.N)<=window))/window,
+                                  Local_GRP = sum(Local_GRP*(seq(.N)<=window))/window,
+                                  National_occ = sum(National_occ*(seq(.N)<=window))/window,
+                                  Local_occ = sum(Local_occ*(seq(.N)<=window))/window),
+                                 by=c("BrandCode", "MarketCode")]
+          simulated = simulated[rep(seq(.N), each=window)]
+          simulated_weekdate = as.Date("01/01/1900",format="%w/%W/%Y")
+          simulated[, `:=`(WeekDate=simulated_weekdate, Week=NA, #Note that simulated data does not have real Week values
+                           National_spend=NA, Local_spend=NA)] 
+          # Assign all markets' codes
+          setcolorder(simulated, c("BrandCode", "MarketCode", "Week", "WeekDate",
+                                   "National_GRP", "Local_GRP", "National_occ", "Local_occ",
+                                   "National_spend", "Local_spend"))
+          setcolorder(Ad_data, c("BrandCode", "MarketCode", "Week", "WeekDate",
+                                 "National_GRP", "Local_GRP", "National_occ", "Local_occ",
+                                 "National_spend", "Local_spend"))
+          Ad_data = rbind(simulated, Ad_data)
+        }
+    
+        # Calculate Ad stock using roll_sum
+        geom_weights = cumprod(c(1.0, rep(weight, times = window)))
+        geom_weights = sort(geom_weights)
+        setkey(Ad_data, BrandCode, MarketCode, Week)
+        Ad_data[,`:=`(National_GRP = roll_sum(National_GRP, n = window+1, weights = geom_weights,
+                                              normalize = F, align = "right", fill = NA),
+                      Local_GRP = roll_sum(Local_GRP, n = window+1, weights = geom_weights,
+                                           normalize = F, align = "right", fill = NA),
+                      National_occ = roll_sum(National_occ, n = window+1, weights = geom_weights,
+                                              normalize = F, align = "right", fill = NA),
+                      Local_occ = roll_sum(Local_occ, n = window+1, weights = geom_weights,
+                                           normalize = F, align = "right", fill = NA)),
+                      by=c("BrandCode", "MarketCode")]
+        Ad_data[, Total_GRP:=rowSums(.SD, na.rm=T), .SDcols=c("National_GRP", "Local_GRP")]
+        Ad_data[, Total_occ:=rowSums(.SD, na.rm=T), .SDcols=c("National_occ", "Local_occ")]
+        Ad_data[, Week:=format(WeekDate,format="%W/%Y")]
+        Ad_data[, WeekDate:=NULL]
   
-      # Calculate Ad stock using cumsum
-      Ad_example[, `:=`(National_GRP = cumsum(ifelse(is.na(National_GRP), 0, National_GRP)*weight^(rev(seq(.N))-1))/
-                                       (weight^(rev(seq(.N))-1)),
-                        Local_GRP = cumsum(ifelse(is.na(Local_GRP), 0, Local_GRP)*weight^(rev(seq(.N))-1))/
-                                    (weight^(rev(seq(.N))-1)),
-                        National_occ = cumsum(ifelse(is.na(National_occ), 0, National_occ)*weight^(rev(seq(.N))-1))/
-                                       (weight^(rev(seq(.N))-1)),
-                        Local_occ = cumsum(ifelse(is.na(Local_occ), 0, Local_occ)*weight^(rev(seq(.N))-1))/
-                                    (weight^(rev(seq(.N))-1))),
-                    by=c("BrandCode", "MarketCode")]
+        # Aggregate over brand match_tiers for RMS_brand
+        Ad_data = Ad_data[,.(National_GRP = sum(National_GRP, na.rm=T),
+                             Local_GRP = sum(Local_GRP, na.rm=T),
+                             Total_GRP = sum(Total_GRP, na.rm=T),
+                             National_occ = sum(National_occ, na.rm=T),
+                             Local_occ = sum(Local_occ, na.rm=T),
+                             Total_occ = sum(Total_occ, na.rm=T),
+                             National_spend = sum(National_spend, na.rm=T),
+                             Local_spend = sum(Local_spend, na.rm=T)), by=c("MarketCode","Week")]
+        Ad_data[, MarketCode:=MarketCode+400] # match with dma_code numbers
   
-      # Implement window=T by subtracting previous values
-      # Under option (a), this will leave the first 52 rows as NA
-      # Under option (b), this will leave simulated data as NA
-      Ad_example[, `:=`(National_GRP = National_GRP - shift(National_GRP, window, type="lag")*weight^window,
-                        Local_GRP = Local_GRP - shift(Local_GRP, window, type="lag")*weight^window,
-                        National_occ = National_occ - shift(National_occ, window, type="lag")*weight^window,
-                        Local_occ = Local_occ - shift(Local_occ, window, type="lag")*weight^window),
-                        by=c("BrandCode", "MarketCode")]
-      Ad_example[, Total_GRP:=rowSums(.SD, na.rm=T), .SDcols=c("National_GRP", "Local_GRP")]
-      Ad_example[, Total_occ:=rowSums(.SD, na.rm=T), .SDcols=c("National_occ", "Local_occ")]
-      Ad_example[, Week:=format(WeekDate,format="%W/%Y")]
-      Ad_example[, WeekDate:=NULL]
-      # Aggregate over brand match_tiers for RMS_brand
-      Ad_example = Ad_example[,.(National_GRP = sum(National_GRP, na.rm=T),
-                                 Local_GRP = sum(Local_GRP, na.rm=T),
-                                 Total_GRP = sum(Total_GRP, na.rm=T),
-                                 National_occ = sum(National_occ, na.rm=T),
-                                 Local_occ = sum(Local_occ, na.rm=T),
-                                 Total_occ = sum(Total_occ, na.rm=T)), by=c("MarketCode","Week")]
-      Ad_example[, MarketCode:=MarketCode+400] # match with dma_code numbers
+        # Join to zip/DMA matching
+        RMS_data = merge(RMS_data, store_dma_mapping, by=c("store_code_uc", "year"))
+        
+        # Join RMS to Ad Intel
+        print("Joining RMS to Ad Intel...")
+        output = merge(RMS_data, Ad_data, by.x=c("dma_code", "Week"),
+                       by.y=c("MarketCode", "Week"), all.x=T) # Keep RMS even if there is no advertising
+        
+        # Drop columns
+        keep_cols = c("brand_code_uc_corrected", "product_module_code", "store_code_uc",
+                      "dma_code", "week_end", "year", "price", "units", "promo_percentage",
+                      "promo_dummy", "Total_GRP", "Total_occ")
+        output = output[, keep_cols, with=F]
   
-      # Join RMS to Ad Intel
-      print("Joining RMS to Ad Intel...")
-      #print(nrow(RMS_example))
-      RMS_example = merge(RMS_example, Ad_example, by.x=c("dma_code", "Week"),
-                     by.y=c("MarketCode", "Week"))
-      #print(nrow(output))
-      
-      # Add address metadata
-      RMS_example[, rollyear:=year]
-      setkey(RMS_example, store_code_uc, rollyear)
-      setkey(store_info, store_code_uc, rollyear)
-      store_info = unique(store_info)
-      RMS_example = store_info[RMS_example, roll=T, rollends=c(T,T)]
-      RMS_example[, c("rollyear", "Year", "DATE"):=NULL]
-      stores[, dma_code:=NULL]
-      RMS_example = merge(RMS_example, stores, by=c("store_code_uc", "year"))
-      RMS_example = merge(RMS_example, zipborders, by.x=c("ZIP"), by.y=c("zip"), allow.cartesian=T)
-      
-      # Drop columns
-      keep_cols = c("brand_code_uc_corrected", "product_module_code", "store_code_uc",
-                    "dma_code", "week_end", "Week", "year", "price", "units", "base_price", "promo_percentage",
-                    "promo_dummy", "National_GRP", "Local_GRP", "Total_GRP", "National_occ",
-                    "Local_occ", "Total_occ", "ZIP", "STR NUM", "STREET", "CITY", "ST", 
-                    "fips_state_code", "fips_county_code", "fips", "on_border", "bordername")
-      output = RMS_example[, keep_cols, with=F]
-  
-      if(nrow(output)>0){
-        print("Saving...")
-        dir.create(file.path(output_dir, dir_name), showWarnings = FALSE)
-        saveRDS(output, file=paste0(file.path(output_dir, toString(dir_name)), "/",
-                                    RMS_brand_code, ".rds"))
+        if(nrow(output)>0){
+          print("Saving...")
+          dir.create(file.path(output_dir, dir_name), showWarnings = FALSE)
+          saveRDS(output, file=paste0(file.path(output_dir, toString(dir_name)), "/",
+                                      RMS_brand_code, ".rds"))
+        }
       }
     } else {
-      print("Ad_example empty")
+      print("Ad_data empty")
     }
   }
 }
