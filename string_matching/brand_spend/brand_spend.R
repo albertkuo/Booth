5be86f1dc40017c950822db7ec21cba72cc01cc3
Albert Kuo <albertkuo@uchicago.edu>
2017-05-22 12:24:17 -0500

    Cleaned up code

---
+++ string_matching/brand_spend/brand_spend.R
@@ -0,0 +1,35 @@
+library(data.table)
+prod_cols = c("BrandCode","BrandDesc","BrandVariant",
+              "AdvParentCode","AdvParentDesc","AdvSubsidCode","AdvSubsidDesc",
+              "ProductID","ProductDesc","PCCSubCode","PCCSubDesc","PCCMajCode",
+              "PCCMajDesc","PCCIndusCode","PCCIndusDesc")
+
+source_dir = "/grpshares/hitsch_shapiro_ads/data/Ad_Intel/aggregated"
+filenames = list.files(pattern="\\.rds$", path=source_dir, full.names=TRUE)
+
+for(filename in filenames){
+  all <- readRDS(filename)
+  # Sum across columns (media types)
+  all[, media_sum := rowSums(.SD, na.rm=T), .SDcols = grep("Spend", names(all))]
+  # Sum across rows (weeks and markets)
+  temp = all[,.(spend_sum = sum(media_sum)), by=prod_cols]
+  
+  if(exists("spend")){
+    spend = merge(temp,spend,by=prod_cols, all=TRUE)
+    spend[is.na(spend)] = 0
+    spend[, spend_sum := spend_sum.x+spend_sum.y]
+    spend[, c('spend_sum.x','spend_sum.y') := NULL]
+  } else {
+    spend = temp
+  }
+}
+saveRDS(spend, "brand_spend.rds")
+
+# Get top ten spend brands in each PCCIndusCode
+spend[, spend_sum:=-spend_sum] # Just to get right order
+setkey(spend, spend_sum)
+topten = spend[, head(.SD, 10), by=c('PCCIndusCode','PCCIndusDesc')]
+spend[, spend_sum:=-spend_sum]
+topten[, spend_sum:=-spend_sum]
+setkey(spend, spend_sum)
+write.csv(topten, file = "brand_spend_topten.csv", row.names=F)
--- string_matching/string_matching.R
+++ string_matching/string_matching.R
@@ -1,20 +1,40 @@
-# Additional data to help with matching
-pccs = fread('~/Booth/string_matching/highlighted_categories.csv')
-pccsubs = unique(pccs$PCCSubDesc)
-productgroupdict = fread('~/Booth/string_matching/productgroupdict.csv')
-pccindusdict = fread('~/Booth/string_matching/pccindusdict.csv')
+# string_matching.R
+# -----------------------------------------------------------------------------
+# Author:             Albert Kuo
+# Date last modified: May 22, 2016
+#
+# This is an R script that creates data files for the Shiny string_matching app
 
-# RMS data
-load('~/Booth/string_matching/Products.RData')
-load('~/Booth/string_matching/Meta-Data.RData')
+## ================
+## Set-up =========
+## ================
+library(data.table)
+library(dplyr)
+library(bit64)
+
+source_dir = "~/Booth/string_matching"
+
+# Additional data to help with matching
+pccs = fread(paste0(source_dir, '/highlighted_categories.csv'))
+pccsubs = unique(pccs$PCCSubDesc)
+productgroupdict = fread(paste0(source_dir, '/productgroupdict.csv'))
+pccindusdict = fread(paste0(source_dir, '/pccindusdict.csv'))
+
+## ================
+## RMS data =======
+## ================
+load(paste0(source_dir, '/Products-Corrected.RData'))
+load(paste0(source_dir, '/Meta-Data-Corrected.RData'))
 prod_meta = merge(products, meta_data)
 prod_meta = prod_meta[department_descr!="GENERAL MERCHANDISE"]
+
 ## Aggregate revenue sums by brands
 brands_RMS = prod_meta[,.(rev_sum = sum(revenue_RMS)), 
-                        by=c("brand_code_uc","brand_descr",
-                             "product_module_descr","product_group_descr",
-                             "department_descr")]
-## Add own category label
+                       by=c("brand_code_uc","brand_descr",
+                            "product_module_descr","product_group_descr",
+                            "department_descr")]
+
+## Add broad category labels from productgroupdict
 brands_RMS = merge(brands_RMS, productgroupdict, 
                    by="product_group_descr", all.x=T)
 brands_RMS = brands_RMS[order(rev_sum,decreasing=T)]
@@ -24,32 +44,32 @@ brands_RMS = brands_RMS[!grepl("CTL BR", brands_RMS$brand_descr)]
 setcolorder(brands_RMS, c("brand_code_uc","brand_descr",
                           "product_module_descr","product_group_descr",
                           "department_descr","category","rev_sum"))
-saveRDS(brands_RMS, '~/Booth/string_matching/string_matching_app/data/brands_RMS.rds')
+saveRDS(brands_RMS, paste0(source_dir, '/string_matching_app/data/brands_RMS.rds'))
 
 ## Save union of top brands per module per year (top competitors)
 top_brands = prod_meta[,.(rev_sum_year = sum(revenue_RMS)),
-                      by=c("brand_code_uc","brand_descr","product_module_descr",
-                           "product_group_descr","department_descr","year")]
+                       by=c("brand_code_uc","brand_descr","product_module_descr",
+                            "product_group_descr","department_descr","year")]
 module_sums = prod_meta[,.(module_sum = sum(revenue_RMS)),
                         by = c("product_module_descr","year")]
 top_brands = merge(top_brands, module_sums, by=c("product_module_descr","year"))
 top_brands[,prop_revenue:=rev_sum_year/module_sum]
-library(dplyr)
 top_brands_names = top_brands %>% 
-                    group_by(product_module_descr, year) %>% 
-                    arrange(desc(prop_revenue)) %>% 
-                    mutate(cum_sum_prop = cumsum(prop_revenue)) %>%
-                    filter(cum_sum_prop < .8 | (cum_sum_prop > .8 & (cum_sum_prop-prop_revenue) < .8)) 
+  group_by(product_module_descr, year) %>% 
+  arrange(desc(prop_revenue)) %>% 
+  mutate(cum_sum_prop = cumsum(prop_revenue)) %>%
+  filter(cum_sum_prop < .8 | (cum_sum_prop > .8 & (cum_sum_prop-prop_revenue) < .8)) 
 top_brands_names = unique(top_brands_names$brand_descr)
 top_brands = top_brands[brand_descr %in% top_brands_names] %>%
-                group_by(brand_descr, add=TRUE) %>% mutate(prop_revenue_avg = mean(prop_revenue)) %>%
-                group_by(brand_descr, product_module_descr) %>% mutate(rev_sum = sum(rev_sum_year)) %>%
-                group_by(product_module_descr) %>% distinct(brand_descr, .keep_all = TRUE) 
+  group_by(brand_descr, add=TRUE) %>% mutate(prop_revenue_avg = mean(prop_revenue)) %>%
+  group_by(brand_descr, product_module_descr) %>% mutate(rev_sum = sum(rev_sum_year)) %>%
+  group_by(product_module_descr) %>% distinct(brand_descr, .keep_all = TRUE) 
 top_brands = merge(top_brands, productgroupdict, by="product_group_descr", all.x=T)
 top_brands = top_brands[c("brand_code_uc","brand_descr","product_module_descr",
                           "product_group_descr","department_descr","category",
                           "rev_sum", "prop_revenue_avg")]
-saveRDS(top_brands, '~/Booth/string_matching/string_matching_app/data/top_brands.rds')
+saveRDS(top_brands, paste0(source_dir, '/string_matching_app/data/top_brands.rds'))
+
 ## Save prod data
 prod_meta = prod_meta[,.(rev_sum = sum(revenue_RMS)),
                       by=c("upc_descr","brand_code_uc","brand_descr",
@@ -58,16 +78,15 @@ prod_meta = prod_meta[,.(rev_sum = sum(revenue_RMS)),
 prod_meta = prod_meta[,.(upc_descr, 
                          brand_code_uc, brand_descr,
                          product_module_descr, product_group_descr,
-                         department_descr,rev_sum)]
+                         department_descr, rev_sum)]
 setkey(prod_meta)
 prod_meta = unique(prod_meta)
-saveRDS(prod_meta, '~/Booth/string_matching/string_matching_app/data/prod_meta.rds')
-## Select top 100 brands
-brands_top_RMS = brands_RMS[1:100]
-brandnames_RMS = brands_top_RMS$brand_descr
+saveRDS(prod_meta, paste0(source_dir, '/string_matching_app/data/prod_meta.rds'))
 
-# Ad Intel Data
-brands_ad = readRDS('~/Booth/brand_spend/brand_spend.rds')
+## ================
+## Ad Intel data ==
+## ================
+brands_ad = readRDS(paste0(source_dir, '/brand_spend/brand_spend.rds'))
 brands_ad = brands_ad[PCCSubDesc %in% pccsubs] # highlighted pccs only
 brands_ad = brands_ad[order(spend_sum,decreasing=T)]
 setkey(brands_ad, BrandDesc, BrandVariant)
@@ -78,12 +97,12 @@ brands_unique_ad = brands_unique_ad[,.(BrandCode,BrandVariant,BrandDesc,ProductD
                                        PCCSubDesc,PCCMajDesc,PCCIndusDesc,
                                        category,spend_sum)]
 PCC_sums = brands_unique_ad[,.(product_sum = sum(spend_sum)),
-                               by=c("ProductDesc")]
+                            by=c("ProductDesc")]
 brands_unique_ad = merge(brands_unique_ad, PCC_sums, by=c("ProductDesc"))
 brands_unique_ad[,prop_revenue:=spend_sum/product_sum]
 brands_unique_ad = brands_unique_ad[,c("BrandCode","BrandVariant","BrandDesc",
-                                      "ProductDesc","PCCSubDesc","PCCMajDesc",
-                                      "PCCIndusDesc","category","spend_sum","prop_revenue"),
+                                       "ProductDesc","PCCSubDesc","PCCMajDesc",
+                                       "PCCIndusDesc","category","spend_sum","prop_revenue"),
                                     with=F]
 brands_unique_ad[,prop_revenue:=round(prop_revenue,3)]
 # Have to convert to ASCII for searching to work in the app
@@ -91,94 +110,4 @@ brands_unique_ad = as.data.frame(brands_unique_ad)
 brands_unique_ad[,sapply(brands_unique_ad,is.character)] <- sapply(
   brands_unique_ad[,sapply(brands_unique_ad,is.character)],
   iconv,"UTF-8","ASCII",sub="")
-saveRDS(brands_unique_ad, '~/Booth/string_matching/string_matching_app/data/brands_ad.rds')
-setDT(brands_unique_ad)
-brandnames_ad = brands_unique_ad$BrandDesc # or use BrandVariant?
-
-# Fuzzy string matching with agrep
-candidates_indices = sapply(brandnames_RMS, function(x){agrep(x, brandnames_ad, fixed=T)})
-candidates_names = sapply(candidates_indices, function(i){brandnames_ad[i]})
-
-# Option 1: sort by string distance 
-# candidates_distances = sapply(1:length(brandnames_RMS), function(i){stringdist(brandnames_RMS[i], candidates_names[[i]])})
-# candidates_indices = sapply(1:length(candidates_indices), function(i){candidates_indices[[i]][order(candidates_distances[[i]])]})
-# Option 2: sort by spend
-candidates_spends = sapply(1:length(brandnames_RMS), function(i){sapply(candidates_indices[[i]],function(j){brands_unique_ad[j,spend_sum]})})
-candidates_indices = sapply(1:length(candidates_indices), function(i){candidates_indices[[i]][order(candidates_spends[[i]], decreasing = T)]})
-# Option 3: sort by mix of both
-# candidates_distances = sapply(1:length(brandnames_RMS), function(i){stringdist(brandnames_RMS[i], candidates_names[[i]])})
-# candidates_spends = sapply(1:length(brandnames_RMS), function(i){sapply(candidates_indices[[i]],function(j){brands_unique_ad[j,spend_sum]})})
-# candidates_indices = sapply(1:length(candidates_indices), function(i){candidates_indices[[i]][
-#   order(rank(candidates_distances[[i]])-rank(candidates_spends[[i]]))]})
-
-# Cap at 20 candidates
-#candidates_indices = sapply(candidates_indices, function(x){if(length(x)>20){x[1:20]}else{x}})
-candidates_names = sapply(candidates_indices, function(i){brandnames_ad[i]})
-names(candidates_names) = brandnames_RMS
-
-#dist.distances = sapply(1:length(brandnames_RMS), function(i){stringdist(brandnames_RMS[i], dist.names[[i]])})
-#dist.names_ordered = sapply(1:length(dist.names), function(i){dist.names[[i]][order(dist.distances[[i]])]})
-#names(dist.names_ordered) = brandnames_RMS
-
-# Create table of candidates
-empty_candidates_table <- function(i){
-  candidates_table = data.table(BrandDesc=NA,ProductDesc=NA,
-                                PCCSubDesc=NA,PCCMajDesc=NA,
-                                PCCIndusDesc=NA,category=NA,spend_sum=NA)
-  candidates_table = cbind(brands_top_RMS[i,], candidates_table)
-  return(candidates_table)
-}
-create_candidates_table <- function(i){
-  # No candidates
-  if(length(candidates_indices[[i]])==0){
-    return(empty_candidates_table(i))
-  }
-  # No. of candidates > 0
-  RMS_brand_category = brands_top_RMS[i,category]
-  candidates_rows = lapply(candidates_indices[[i]], function(j)
-    {brands_unique_ad[j,list(BrandDesc,ProductDesc,PCCSubDesc,
-                             PCCMajDesc,PCCIndusDesc,category,spend_sum)]})
-  candidates_table = Reduce(rbind, candidates_rows)
-  candidates_table = candidates_table[category==RMS_brand_category] # Match category (broad category)
-  if(nrow(candidates_table)==0){
-    return(empty_candidates_table(i))
-  } else{
-    candidates_table = cbind(brands_top_RMS[rep(i,nrow(candidates_table)),], candidates_table)
-    return(candidates_table)
-  }
-}
-
-candidates_tables = lapply(1:length(candidates_indices),create_candidates_table)
-all_candidates = Reduce(rbind, candidates_tables)
-write.csv(all_candidates, "all_candidates.csv", row.names=F)
-
-# Match groups
-# groups_RMS = unique(prod_meta$product_group_descr)
-# groups_RMS = groups_RMS[groups_RMS!=""]
-# modules_RMS = unique(prod_meta$product_module_descr)
-# modules_RMS = modules_RMS[modules_RMS!=""]
-# groups_ad = unique(brands_ad$PCCSubDesc)
-# groups.names = sapply(groups_RMS, function(x){agrep(x, groups_ad, value=T, ignore.case=T)})
-# modules.names = sapply(modules_RMS, function(x){agrep(x, groups_ad, value=T, ignore.case=T)})
-# Method 2: using the native R adist, only works for min.name
-# dist.name = adist(brandnames_RMS, brandnames_ad)
-# dist.name=t(apply(dist.name, 1, sort))
-# 
-# #min.name = apply(dist.name, 1, min)
-#
-# match.s1.s2<-NULL  
-# for(i in 1:nrow(dist.name))
-# {
-#     s2.ij<-match(min.name[i],dist.name[i,])
-#     s1.i<-i
-#     match.s1.s2 <- rbind(data.frame(ad.ij=s2.ij,rms.i=s1.i,
-#                                   ad_name=brandnames_ad[s2.ij], 
-#                                   rms_name=brandnames_RMS[s1.i], 
-#                                   adist=top.names[i,j]),match.s1.s2)
-# }
-# # and we then can have a look at the results
-# 
-# View(match.s1.s2)
-
-
-
+saveRDS(brands_unique_ad, paste0(source_dir, '/string_matching_app/data/brands_ad.rds'))
