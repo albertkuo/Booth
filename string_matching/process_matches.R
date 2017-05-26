be4ec3984f228db7f69ccb8f712fcc938dfeee82
Albert Kuo <albertkuo@uchicago.edu>
2017-05-25 19:26:43 -0500

    Fixed paths and column order

--- string_matching/process_matches.R
+++ string_matching/process_matches.R
@@ -13,13 +13,15 @@ library(bit64)
 
 source_dir = "./string_matching"
 
-# Process matches to get product_module_code (only do this once and then save string_matches file)
-string_matches = fread(paste0(source_dir, "/matches_files/all.csv"))
-load(paste0(source_dir, "/Products-Corrected.RData"))
+# Process matches to get product_module_code
+load(paste0(source_dir, "/data/Products-Corrected.RData"))
 products = products[, c("product_module_code", "product_module_descr"), with=F]
 setkey(products, product_module_code)
 products = unique(products)
+
+string_matches = fread(paste0(source_dir, "/matches_files/all.csv"))
 string_matches = string_matches[, c("brand_code_uc", "product_module_descr", "match_tier", "BrandCode"), with=F]
 string_matches = merge(string_matches, products, by="product_module_descr")
 string_matches[, product_module_descr:=NULL]
-write.csv(string_matches, paste0(source_dir, "/string_matches.csv")
+setcolorder(string_matches, c("brand_code_uc", "product_module_code", "match_tier", "BrandCode"))
+write.csv(string_matches, paste0(source_dir, "/string_matches.csv"), row.names=F)
